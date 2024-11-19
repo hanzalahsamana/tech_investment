ebad9694674af01ce46c2eb6d1a8228e6dd3383e
ubaidullahshahid <146563446+ubaidullahshahid@users.noreply.github.com>
2024-11-20 03:20:59 +0500

    solve warnings

--- src/App.js
+++ src/App.js
@@ -7,17 +7,16 @@ import Ourfeatures from '../src/components/ourfeatures/index.jsx';
 import Ourteam from '../src/components/ourteam';
 import Contact from '../src/components/Contact';
 import Footer from '../src/components/footer';
-import { Helmet } from 'react-helmet';
 import 'animate.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'owl.carousel/dist/assets/owl.carousel.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import './css/bootstrap.min.css';
-import './css/bootstrap.css';
+// import './css/bootstrap.min.css';
+// import './css/bootstrap.css';
 import './css/main.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
 const HeadSection = () => {
--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -24,13 +24,13 @@ function Navbar() {
     <header className={`bg-light custom-header`}>
       <nav className={`navbar navbar-expand-lg navbar-light bg-light my-new-class ${isScrolled ? 'fixed-top' : ''}`} id="header-nav" role="navigation">
         <div className="container">
-          <a className="link-dark navbar-brand site-title mb-0" href="#"><img src={logo}className='logo-image' width={'150px'} height={'100px'}/> 607DevLabs</a>
+          <a className="link-dark navbar-brand site-title mb-0" href="#about"><img alt="logo" src={logo}className='logo-image' width={'150px'} height={'100px'}/> Tech Invest</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto me-2">
-              <li className="nav-item"><a className="nav-link" href="#">Home</a></li>
+              <li className="nav-item"><a className="nav-link" href="#about">Home</a></li>
               <li className="nav-item"><a className="nav-link" href="#about">About</a></li>
               <li className="nav-item"><a className="nav-link" href="#services">Services</a></li>
               <li className="nav-item"><a className="nav-link" href="#contact">Contact</a></li>
